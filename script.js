43886bef2f2b6137214e8730a264e5d83e8f70b5
Chandhuru3493 <chandhuruvkp@gmail.com>
2024-07-09 15:37:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementById('Birth').addEventListener('click',function(){
+    document.getElementById("image").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwO9sVo6QB-SEbm4mXlz5t5DZ_IrBKh1KKToUxKXrkykwTbc2xBVyd9EwHiAfSfLmvryo&usqp=CAU"
+});
+document.getElementById('Death').addEventListener('click',function(){
+    document.getElementById("image").src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzu4RBq2aCd8as4HBwZ9E6hmyR5RL7xP1mTQ&s"
+});
+
+
+
+
+
+
+//var a=parseInt(prompt("Enter the num"));
+//document.getElementById("js1");
+//document.write(9+4);
+//window.alert("web site is hacked");
+//console.log("helow i am secret message")
+//comparison operator <,>,
